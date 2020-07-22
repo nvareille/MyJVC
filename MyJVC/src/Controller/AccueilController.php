4f5344e219dd595469af6af25369fb3e67cec103
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2020-07-22 11:07:07 +0200

    Ajout d'images dans AccueilController

--- MyJVC/src/Controller/AccueilController.php
+++ MyJVC/src/Controller/AccueilController.php
@@ -22,7 +22,12 @@ class AccueilController extends AbstractController
 				'Raphaël',
 				'Guenole',
 				'Alexandre'
-			]
+			],
+            'images' => [
+                'https://www.pokepedia.fr/images/thumb/1/17/Dracaufeu-RFVF.png/250px-Dracaufeu-RFVF.png',
+                'https://www.pokepedia.fr/images/thumb/4/42/Florizarre-RFVF.png/250px-Florizarre-RFVF.png',
+                'https://www.pokepedia.fr/images/thumb/2/24/Tortank-RFVF.png/250px-Tortank-RFVF.png'
+            ]
         ]);
     }
 	
